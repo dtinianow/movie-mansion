a1419363b8dce9c5b6ba37d69419b3f618f24e58
David Tinianow <david.tinianow@gmail.com>
2017-02-26 15:14:24 -0700

    Create test user can view movie showtimes by date

---
+++ spec/features/user_can_view_showtimes_for_all_movies_by_date_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature 'User can view showtimes for all movies by date' do
+  scenario 'they see all movie showtimes on the current date by default' do
+    movie = create(:movie)
+    auditorium = create(:auditorium)
+    showtime = create(:showtime)
+    current_date = DateTime.now.strftime('%m/%d/%Y')
+
+    visit showtimes_path
+
+    within('#showtimes-header') do
+      expect(page).to have_content "Movies Playing On #{current_date}"
+    end
+
+    within('#showtimes-table tbody tr:nth-child(1)') do
+      expect(page).to have_content(movie.title)
+      expect(page).to have_content(movie.minutes)
+      expect(page).to have_content(movie.rating)
+      expect(page).to have_content(movie.image_url)
+      expect(page).to have_content(auditorium.title)
+      expect(page).to have_content(showtime.start_time)
+      expect(page).to have_content(showtime.price)
+    end
+  end
+end
