1aa706efa55a0d8f06a2a5f064dbf1e267995dd2
David Tinianow <david.tinianow@gmail.com>
2017-02-26 15:02:00 -0700

    Setup initial factories

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,3 +1,28 @@
 FactoryGirl.define do
-  # <factories for each model go here>
+  factory :movie do
+    title 'Ex Machina'
+    minutes 108
+    rating 'R'
+    image_url 'https://c1.staticflickr.com/8/7649/17097191625_77c28cf79b_b.jpg'
+  end
+
+  factory :auditorium do
+    title 'A'
+    capacity 160
+  end
+
+  factory :showtime do
+    movie
+    auditorium
+    start_time DateTime.now
+    # Add movie length time in minutes converted into seconds
+    end_time { DateTime.now + (60 * "#{movie.minutes}".to_i) }
+    price 10.00
+    tickets_available 120
+  end
+
+  factory :order do
+    name 'David'
+    email 'david@example.com'
+  end
 end
